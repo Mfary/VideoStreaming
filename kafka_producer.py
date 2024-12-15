28ec8bf5ecefba65260608f4e4170c3d3dc36413
Mohammad <mfarahani1379@gmail.com>
2024-12-15 09:55:38 -0800

    Update kafka_producer.py

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,4 +1,5 @@
 from kafka import KafkaProducer
+import base64
 import cv2
 import time
 
@@ -7,14 +8,14 @@ TOPIC = 'video_topic'
 VIDEO_FILE = 'input_video.mp4'
 
 def video_stream_to_kafka(video_path):
-    producer = KafkaProducer(bootstrap_servers=BROKER)
+    producer = KafkaProducer(bootstrap_servers=BROKER, value_serializer=lambda v: str(v).encode('utf-8'))
     cap = cv2.VideoCapture(video_path)
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
         _, buffer = cv2.imencode('.jpg', frame)
-        producer.send(TOPIC, buffer.tobytes())
+        producer.send(TOPIC, base64.b64encode(buffer).decode('utf-8'))
         time.sleep(0.03)
     cap.release()
     producer.close()
